a1b7e1618ee6ea40292dac84e75589747103de91
adasgup4 <abhideep.dasgupta@gmail.com>
2018-02-05 23:12:00 -0500

    sad because no https

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-alert('Random Javascript, updated!');
+(function() {
+  'use strict';
+
+  var app = {
+    isLoading: true,
+    visibleCards: {},
+    selectedCities: [],
+    spinner: document.querySelector('.loader'),
+    cardTemplate: document.querySelector('.cardTemplate'),
+    container: document.querySelector('.main'),
+    addDialog: document.querySelector('.dialog-container'),
+    daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  };
+
+
+  /*****************************************************************************
+   *
+   * Event listeners for UI elements
+   *
+   ****************************************************************************/
+
+  document.getElementById('butCall').addEventListener('click', function() {
+    // Refresh all of the forecasts
+    app.makeCall();
+  });
+
+  document.getElementById('butEmail').addEventListener('click', function() {
+    // Open/show the add new city dialog
+    app.sendEmail(true);
+  });
+
+
+  /*****************************************************************************
+   *
+   * Methods to update/refresh the UI
+   *
+   ****************************************************************************/
+
+  
+
+  if (app.isLoading) {
+    app.spinner.setAttribute('hidden', true);
+    app.container.removeAttribute('hidden');
+    app.isLoading = false;
+  }
+
+  // TODO add startup code here
+
+  // TODO add service worker code here
+})();
